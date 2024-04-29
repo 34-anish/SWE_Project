73fdf3e33f0de4b465c6ffa9ddb50d1d4ca8edc4
Ankush Niroula <ankushniroulaa@gmail.com>
2024-04-29 08:47:11 +0530

    First Draft

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,15 @@
-import { Box, Menu,  MenuItem, MenuList, Input, Spacer, Text, Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Box, Input, Spacer, Text, Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
-import { AiFillHome } from "react-icons/ai";
 import { RxAvatar } from "react-icons/rx";
 import { Link as RouterLink } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
 import useLogout from "../hooks/useLogout";
-import authScreenAtom from "../atoms/authAtom";
 import useShowToast from "../hooks/useShowToast";
 import { BsFillChatQuoteFill } from "react-icons/bs";
 import { MdOutlineSettings } from "react-icons/md";
-// import { FiSearch as Search } from 'react-icons/fi';
 import { useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -20,38 +17,26 @@ const Header = () => {
 	const { colorMode, toggleColorMode } = useColorMode();
 	const user = useRecoilValue(userAtom);
 	const logout = useLogout();
-	const setAuthScreen = useSetRecoilState(authScreenAtom);
+	const searchResultsRef = useRef();
 	const [searchText, setSearchText] = useState("");
 	const showToast = useShowToast();
 	const [searchingUser, setSearchingUser] = useState(false);
 	const [searchResults, setSearchResults] = useState([]);
 	const navigate = useNavigate();
 	
-	// const handleInputChange = (event) => {
-	// 	setSearchTerm(event.target.value);
-	// 	onSearch(event.target.value);
-	// };
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+		  if (searchResultsRef.current && !searchResultsRef.current.contains(event.target)) {
+			setSearchResults([]);
+		  }
+		};
+	  
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+		  document.removeEventListener('mousedown', handleClickOutside);
+		};
+	  }, []);
 
-	// const onSearch = async (value) => {
-	// 	const response = await fetch(`/profile/${value}`);
-	// 	if (response.ok) {
-	// 	  const user = await response.json();
-	// 	  navigate(`/profile/${user.username}`);
-	// 	} else {
-	// 	  console.log('User not found');
-	// 	}
-	//   };
-	
-	// const handleFormSubmit = async (event) => {
-	// 	event.preventDefault();
-	// 	const response = await fetch(`/profile/${searchTerm}`);
-	// 	if (response.ok) {
-	// 	  const user = await response.json();
-	// 	  navigate(`/profile/${user.username}`);
-	// 	} else {
-	// 	  console.log('User not found');
-	// 	}
-	// };
 	const handleSearch = async(e)=>{
 		e.preventDefault();
 		try{
@@ -73,24 +58,6 @@ const Header = () => {
 
 	return (
 		<Flex justifyContent={"center"} mt={6} mb='12'>
-			{/* {user && (
-				<Link as={RouterLink} to='/'>
-					<AiFillHome size={24} />
-				</Link>
-			)}
-			{!user && (
-				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("login")} color= {"blackAlpha.700"}>
-					Login
-				</Link>
-			)} */}
-
-			{/* <Image
-				cursor={"pointer"}
-				alt='logo'
-				w={6}
-				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-				onClick={toggleColorMode}
-			/> */}
 			{
 			<Text
 				cursor={"pointer"}
@@ -101,15 +68,8 @@ const Header = () => {
 				Connecta
 			</Text>
 			}
-			<Spacer />
-			{/* <form onSubmit={handleSearch}>
-				<Input 
-					value={searchText}
-					onChange={(e) => setSearchText(e.target.value)}
-					placeholder="Search user"
-				/>
-			</form> */}
-			{user && (<Box position="relative">
+			{user && <Spacer />}
+			{user && (<Box position="relative" ref={searchResultsRef}>
 				<form onSubmit={handleSearch}>
 					<Input 
 					value={searchText}
@@ -143,15 +103,15 @@ const Header = () => {
 				)}
 			</Box>
 		)}
-			{<Spacer />}	
+			{user && (<Spacer />)}	
 			{user && (
 				<Flex alignItems={"center"}  gap={4}>
 					<Link as={RouterLink} to={`/${user.username}`} color={'blackAlpha.900'}>
 						<RxAvatar size={24} />
 					</Link>
-					<Link as={RouterLink} to={`/chat`} color={'blackAlpha.900'}>
+					{/* <Link as={RouterLink} to={`/chat`} color={'blackAlpha.900'}>
 						<BsFillChatQuoteFill size={20} />
-					</Link>
+					</Link> */}
 					<Link as={RouterLink} to={`/settings`} color={'blackAlpha.900'}>
 						<MdOutlineSettings size={20} />
 					</Link>
@@ -161,11 +121,6 @@ const Header = () => {
 				</Flex>
 			)}
 
-			{/* {!user && (
-				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("signup")} color= {"blackAlpha.700"}>
-					Sign up
-				</Link>
-			)} */}
 		</Flex>
 	);
 };
